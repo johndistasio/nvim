dea39b2e1f0b28d48e2dc07a86e08cc4e3fb3f96
John Distasio <john@idiotvilla.net>
2026-03-08 20:32:14 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- Set <space> as the global leader key.
+vim.g.mapleader = ' '
+
+-- Set <space> as the buffer-local leader key.
+-- This could be set to something different to have different leader key shortcuts for filetype plugins.
+vim.g.maplocalleader = ' '
+
